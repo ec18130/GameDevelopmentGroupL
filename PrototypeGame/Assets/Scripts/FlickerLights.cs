5a35df9395981065d3a9358f09ec020765611d3a
ec18130 <ec18130@qmul.ac.uk>
2020-10-29 20:12:30 +0000

    Door and Light

---
+++ PrototypeGame/Assets/Scripts/FlickerLights.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLights : MonoBehaviour
+{
+    bool isOn = false;
+    float flickerDelay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOn == false)
+        {
+            StartCoroutine(FlickeringLights());
+        }
+    }
+
+    IEnumerator FlickeringLights()
+    {
+        isOn = true;
+        this.gameObject.GetComponent<Light>().enabled = false;
+        flickerDelay = Random.Range(0.1f, 0.3f);
+        yield return new WaitForSeconds(flickerDelay);
+        this.gameObject.GetComponent<Light>().enabled = true;
+        flickerDelay = Random.Range(0.1f, 0.3f);
+        yield return new WaitForSeconds(flickerDelay);
+        isOn = false;
+    }
+}
+
