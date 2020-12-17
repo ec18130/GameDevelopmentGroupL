fe57aef90e811083a0e256dcc86df693b4761a49
ec18130 <ec18130@qmul.ac.uk>
2020-12-17 09:57:11 +0000

    MainMenu button fix

---
+++ PrototypeGame/Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
