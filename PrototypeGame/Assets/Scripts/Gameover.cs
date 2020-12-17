9535b7b3f66a63dcc44a6130f50eafc98283b5e5
ec18130 <ec18130@qmul.ac.uk>
2020-12-17 10:28:40 +0000

    GameOverScreen with sound

---
+++ PrototypeGame/Assets/Scripts/Gameover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gameover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(10));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
